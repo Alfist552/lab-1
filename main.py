f2a513a282edd9a0bf599d896729059b58844fd6
Alfist552 <ndetsura@mail.ru>
2025-11-24 19:35:10 +0300

    Замена "#" на """ для соответствия стилю PEP8

--- main.py
+++ main.py
@@ -5,22 +5,23 @@ from datetime import datetime
 from enum import Enum
 
 class FileFormat(Enum):
-    # Форматы файлов которые поддерживает редактор
+    """ Форматы файлов которые поддерживает редактор """
     TXT = "txt"
     JSON = "json"
     XML = "xml"
+
 class TextEditorError(Exception):
-    #Исключение для всех ошибок редактора
+    """Исключение для всех ошибок редактора"""
     pass
 
 class DocumentNotFoundError(Exception):
-    #Файл не найден
+    """Файл не найден"""
     pass
 
 class FileOperationError(Exception):
-    #Ошибка чтения/записи
+    """Ошибка чтения/записи"""
     pass
 
 class InvalidPositionError(TextEditorError):
-    #Неправильная позиция курсора(Например, когда курсор пытаются поставить в несуществующее место
+    """Неправильная позиция курсора(Например, когда курсор пытаются поставить в несуществующее место"""
     pass
