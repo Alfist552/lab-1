c8d498d5534736e60b881cbdbc12e35583ebb9be
Alfist552 <ndetsura@mail.ru>
2025-11-24 19:24:26 +0300

    Было добавлено перечисление форматов файлов("txt", "json", "xml")

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import xml.etree.ElementTree as ET
 from typing import List, Dict, Optional, Any, Union
 from datetime import datetime
 from enum import Enum
+
+class FileFormat(Enum):
+    # Форматы файлов которые поддерживает редактор
+    TXT = "txt"
+    JSON = "json"
+    XML = "xml"
